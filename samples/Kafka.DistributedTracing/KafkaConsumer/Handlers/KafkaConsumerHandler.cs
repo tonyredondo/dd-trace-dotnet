8ad8a3d38580bd2b868eeea04e97324f9539ee8a
Zach Montoya <zach.montoya@datadoghq.com>
2021-03-03 11:24:26 -0800

    Work in progress

--- samples/Kafka.DistributedTracing/KafkaConsumer/Handlers/KafkaConsumerHandler.cs
+++ samples/Kafka.DistributedTracing/KafkaConsumer/Handlers/KafkaConsumerHandler.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Confluent.Kafka;
+using Datadog.Trace;
 using Microsoft.Extensions.Hosting;
 
 namespace KafkaConsumer.Handlers
@@ -26,8 +28,71 @@ namespace KafkaConsumer.Handlers
                 {
                     while (true)
                     {
+                        // Receive message
                         var consumer = builder.Consume(cancelToken.Token);
+
+                        // Do original message processing
                         Console.WriteLine($"Message: {consumer.Message.Value} received from {consumer.TopicPartitionOffset}");
+
+                        // Let's add a Span to capture this event
+                        // Read the basic property headers and extract the Datadog properties
+                        var headers = consumer.Message?.Headers;
+                        ulong? parentSpanId = null;
+                        ulong? traceId = null;
+                        SamplingPriority? samplingPriority = null;
+
+                        if (headers != null)
+                        {
+                            // Parse parentId header
+                            if (headers.TryGetLastBytes(HttpHeaderNames.ParentId, out byte[] parentSpanIdBytes))
+                            {
+                                if (ulong.TryParse(Encoding.UTF8.GetString(parentSpanIdBytes), out ulong result))
+                                {
+                                    parentSpanId = result;
+                                }
+                            }
+
+                            // Parse traceId header
+                            if (headers.TryGetLastBytes(HttpHeaderNames.TraceId, out byte[] traceIdBytes))
+                            {
+                                if (ulong.TryParse(Encoding.UTF8.GetString(traceIdBytes), out ulong result))
+                                {
+                                    traceId = result;
+                                }
+                            }
+
+                            // Parse samplingPriority header
+                            if (headers.TryGetLastBytes(HttpHeaderNames.SamplingPriority, out byte[] samplingPriorityBytes))
+                            {
+                                var samplingPriorityString = Encoding.UTF8.GetString(samplingPriorityBytes);
+                                if (Enum.TryParse<SamplingPriority>(samplingPriorityString, out var result))
+                                {
+                                    samplingPriority = result;
+                                }
+                            }
+                        }
+
+                        // Create a new SpanContext to represent the distributed tracing information
+                        SpanContext propagatedContext = null;
+                        if (parentSpanId.HasValue && traceId.HasValue)
+                        {
+                            propagatedContext = new SpanContext(traceId, parentSpanId.Value, samplingPriority);
+                        }
+
+                        // Create the span that is a distributed trace
+                        using (Scope scope = Tracer.Instance.StartActive("kafka.message", propagatedContext))
+                        {
+                            // Set Datadog tags
+                            Span span = scope.Span;
+                            span.ResourceName = $"consume {topic}";
+                            span.Type = SpanTypes.Queue;
+                            span.SetTag(Tags.SpanKind, SpanKinds.Consumer);
+                            span.SetTag("kafka.topic", consumer.Topic);
+                            span.SetTag("message.length", consumer.Message.Value?.Length.ToString() ?? "0");
+
+                            // Do work inside the Datadog trace
+                            Thread.Sleep(500);
+                        }
                     }
                 }
                 catch (Exception)
--- samples/Kafka.DistributedTracing/KafkaProducer/Controllers/KafkaProducerController.cs
+++ samples/Kafka.DistributedTracing/KafkaProducer/Controllers/KafkaProducerController.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Text;
+using System.Threading;
 using Confluent.Kafka;
+using Datadog.Trace;
 using Microsoft.AspNetCore.Mvc;
 
 namespace KafkaProducer.Controllers
@@ -23,9 +26,44 @@ namespace KafkaProducer.Controllers
             {
                 try
                 {
-                    return producer.ProduceAsync(topic, new Message<Null, string> { Value = message })
-                        .GetAwaiter()
-                        .GetResult();
+                    DeliveryResult<Null, string> deliveryResult = null;
+                    using (var scope = Tracer.Instance.StartActive("kafka.message"))
+                    {
+                        // Define the message
+                        var kafkaMessage = new Message<Null, string> { Value = message };
+
+                        // Add properties to the Headers dictionary in the following way:
+                        //  - "x-datadog-trace-id": "<trace_id>"
+                        //  - "x-datadog-parent-id": "<span_id>"
+                        //  - "x-datadog-sampling-priority": "<sampling_priority>"
+                        if (kafkaMessage.Headers == null)
+                        {
+                            kafkaMessage.Headers = new Headers();
+                        }
+
+                        kafkaMessage.Headers.Add(HttpHeaderNames.ParentId, Encoding.UTF8.GetBytes(scope.Span.SpanId.ToString()));
+                        kafkaMessage.Headers.Add(HttpHeaderNames.TraceId, Encoding.UTF8.GetBytes(scope.Span.TraceId.ToString()));
+                        kafkaMessage.Headers.Add(HttpHeaderNames.SamplingPriority, Encoding.UTF8.GetBytes(scope.Span.GetTag(Tags.SamplingPriority)));
+                        kafkaMessage.Headers.Add(HttpHeaderNames.TraceId, Encoding.UTF8.GetBytes(scope.Span.TraceId.ToString()));
+
+                        // Do work inside the Datadog trace
+                        Thread.Sleep(500);
+
+                        // Produce message
+                        deliveryResult = producer.ProduceAsync(topic, kafkaMessage)
+                            .GetAwaiter()
+                            .GetResult();
+
+                        // Set Datadog tags
+                        Span span = scope.Span;
+                        span.ResourceName = $"produce {topic}";
+                        span.Type = SpanTypes.Queue;
+                        span.SetTag(Tags.SpanKind, SpanKinds.Producer);
+                        span.SetTag("kafka.topic", topic);
+                        span.SetTag("message.length", message?.Length.ToString() ?? "0");
+                    }
+
+                    return deliveryResult;
                 }
                 catch (Exception e)
                 {
