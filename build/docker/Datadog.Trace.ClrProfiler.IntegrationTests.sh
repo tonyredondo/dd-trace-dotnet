91e201e186a6ac2ad69dd36d6f5c138bd4e6503a
Kevin Gosse <krix33@gmail.com>
2021-03-29 18:08:38 +0200

    Try enabling coverage on Linux

--- build/docker/Datadog.Trace.ClrProfiler.IntegrationTests.sh
+++ build/docker/Datadog.Trace.ClrProfiler.IntegrationTests.sh
@@ -6,6 +6,10 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"/../../
 mkdir -p /var/log/datadog/dotnet
 touch /var/log/datadog/dotnet/dotnet-tracer-native.log
 
+mkdir -p /var/log/datadog/cover
+
+dotnet tool install -g coverlet.console
+
 dotnet vstest test/Datadog.Trace.IntegrationTests/bin/$buildConfiguration/$publishTargetFramework/publish/Datadog.Trace.IntegrationTests.dll --logger:trx --ResultsDirectory:test/Datadog.Trace.IntegrationTests/results
 
 dotnet vstest test/Datadog.Trace.OpenTracing.IntegrationTests/bin/$buildConfiguration/$publishTargetFramework/publish/Datadog.Trace.OpenTracing.IntegrationTests.dll --logger:trx --ResultsDirectory:test/Datadog.Trace.OpenTracing.IntegrationTests/results
@@ -20,3 +24,4 @@ wait-for-it postgres:5432 -- \
 dotnet vstest test/Datadog.Trace.ClrProfiler.IntegrationTests/bin/$buildConfiguration/$publishTargetFramework/publish/Datadog.Trace.ClrProfiler.IntegrationTests.dll --logger:trx --ResultsDirectory:test/Datadog.Trace.ClrProfiler.IntegrationTests/results
 
 cp /var/log/datadog/dotnet/dotnet-tracer-native.log /project/
+cp -R /var/log/datadog/cover /project/
