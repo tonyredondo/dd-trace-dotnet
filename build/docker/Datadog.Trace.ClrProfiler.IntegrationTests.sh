69a3950e8efedebaf99e986e1727f0a2c9b223d2
Kevin Gosse <krix33@gmail.com>
2021-04-01 10:50:38 +0200

    Fix variable check

--- build/docker/Datadog.Trace.ClrProfiler.IntegrationTests.sh
+++ build/docker/Datadog.Trace.ClrProfiler.IntegrationTests.sh
@@ -8,7 +8,7 @@ touch /var/log/datadog/dotnet/dotnet-tracer-native.log
 
 mkdir -p /var/log/datadog/cover
 
-if [ ! -z "$TEST_COVERAGE" ] 
+if [[ ! -v "$TEST_COVERAGE" ]] And [[ ! -z "$TEST_COVERAGE" ]]
 then
   dotnet tool install -g coverlet.console
   export PATH="$PATH:/root/.dotnet/tools" 
